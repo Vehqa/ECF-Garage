9ab02c5a355a5f967bdc0718596ec650957907f5
Vehqa <pro.nectoux@gmail.com>
2023-07-20 17:15:34 +0200

    Ajout annexes, composer.

--- src/Views/services/index.php
+++ src/Views/services/index.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:title" content="V.Parrot Garage">
+    <meta property="og:url" content="https://www.VParrot.com/">
+    <meta name="description" content="Venez decouvrir notre garage, nos occasions et nos services."/>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Aref+Ruqaa&family=Orbitron:wght@500;600;800&family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
